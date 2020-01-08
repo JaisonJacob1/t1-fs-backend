ce75bfb5a22ab5cb7bee16d057e47c62a4a50d04
JaisonJacob1 <32695446+JaisonJacob1@users.noreply.github.com>
2020-01-08 17:27:20 +0530

    Adding CORS filter (#8)

--- src/main/java/com/bulletinboard/Starter.java
+++ src/main/java/com/bulletinboard/Starter.java
@@ -2,7 +2,11 @@ package com.bulletinboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @ComponentScan
 @SpringBootApplication
@@ -11,5 +15,15 @@ public class Starter {
     public static void main (String args[])
     {
         SpringApplication.run(Starter.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/bulletinBoard/**").allowedOrigins("*");
+            }
+        };
     }
 }
